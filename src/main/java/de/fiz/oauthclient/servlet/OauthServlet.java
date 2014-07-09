9171d5fca65d3e95de64c1fd68ba313828c39521
mih <michael.hoppe@fiz-karlsruhe.de>
2014-07-09 09:43:02 +0200

    initial project-version

---
+++ src/main/java/de/fiz/oauthclient/servlet/OauthServlet.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2014 Michael Hoppe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.fiz.oauthclient.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
+import org.apache.oltu.oauth2.client.response.OAuthAuthzResponse;
+import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
+
+/**
+ * @author mih
+ */
+public class OauthServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        String clientId = "larch";
+        String clientSecret = "secret";
+
+        if (request.getParameter("method").equals("eins")) {
+            try {
+                OAuthClientRequest oauthRequest = OAuthClientRequest
+                        .authorizationLocation("http://localhost:8080/oauth/authorize")
+                        .setClientId(clientId)
+                        .setResponseType("code")
+                        .setRedirectURI("http://localhost:8088/oauthclient/oauth?method=zwei")
+                        .buildQueryMessage();
+                response.sendRedirect(oauthRequest.getLocationUri());
+
+            } catch (OAuthSystemException e) {
+                throw new IOException(e.getMessage());
+            }
+        }
+        else if (request.getParameter("method").equals("zwei")) {
+            try {
+                OAuthAuthzResponse oar = OAuthAuthzResponse.oauthCodeAuthzResponse(request);
+                String code = oar.getCode();
+
+                CloseableHttpClient httpclient = HttpClients.createDefault();
+                HttpPost httpPost = new HttpPost("http://localhost:8080/oauth/token");
+                List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+                nvps.add(new BasicNameValuePair("grant_type", "authorization_code"));
+                nvps.add(new BasicNameValuePair("client_id", clientId));
+                nvps.add(new BasicNameValuePair("client_secret", clientSecret));
+                nvps.add(new BasicNameValuePair("code", code));
+                nvps.add(new BasicNameValuePair("redirect_uri", "http://localhost:8088/oauthclient/oauth?method=zwei"));
+                httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+                String authorization = clientId + ":" + clientSecret;
+                byte[] encodedBytes;
+                encodedBytes = Base64.encodeBase64(authorization.getBytes());
+                authorization = "Basic " + new String(encodedBytes);
+                httpPost.setHeader("Authorization", authorization);
+                CloseableHttpResponse response2 = httpclient.execute(httpPost);
+
+                String test = null;
+                try {
+                    System.out.println(response2.getStatusLine());
+                    HttpEntity entity2 = response2.getEntity();
+                    test = EntityUtils.toString(entity2);
+                    System.out.println(test);
+                } finally {
+                    response2.close();
+                }
+
+                PrintWriter out = response.getWriter();
+                out.println("<html>");
+                out.println("<body>");
+                out.println(test);
+                out.println("</body>");
+                out.println("</html>");
+            } catch (OAuthProblemException e) {
+                throw new IOException(e.getMessage());
+            }
+        }
+        else if (request.getParameter("method").equals("drei")) {
+            PrintWriter out = response.getWriter();
+            out.println("<html>");
+            out.println("<body>");
+            out.println("<h1>drei</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+}
